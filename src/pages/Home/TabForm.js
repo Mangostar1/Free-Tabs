e848c652be42b5c18b982b840b944c1d174e0fdc
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-01-31 21:33:22 -0300

    se agrega bloque trycatch en BassTabForm para controlar errores y usar expresiones regulares

--- src/pages/Home/TabForm.js
+++ src/pages/Home/TabForm.js
@@ -34,36 +34,38 @@ export function BassTabForm() {
     const [count, setCount] = useState(0);
 
     const sendNotes = () => {
-        const strings = document.querySelectorAll('.strings');
-        const $tabRoot = document.querySelector('.tab-root');
-
-        let $bassTab;
-        
-        if (count === 0) {//<-- create the first $tabRoot without duplicating them
-            createBassTab($tabRoot, 1);//<-- create an article and insert it inside $tabRoot
-            setCount(1)
-        }
-        
-        $bassTab = document.querySelector('.bass-tab');//<-- create by createBassTab() in scripts/createBassTab.js
-        
-        if (count === 1) {
-            addBassNotes($bassTab, strings, 'A');//<-- article inside of section ".tab-root", "strings", "second class" (important for create a second article and continue adding more notes on the tab)
-        }
-
-        data.forEach(({ className, nextClassName, countState, id }) => {
-            if (document.querySelector(`.${className}`).textContent.length >= 41) {
-                if (count === countState) {
-                createBassTab($tabRoot, id);
-                setCount(id)
-                }
-                addBassNotes(document.getElementById(id), strings, nextClassName);
+        try {
+            const strings = document.querySelectorAll('.strings');
+            const $tabRoot = document.querySelector('.tab-root');
+            
+            if (count === 0) {//<-- create the first $tabRoot without duplicating them
+                createBassTab($tabRoot, 1);//<-- create an article and insert it inside $tabRoot
+                setCount(1)
             }
-        });
-
-        // Clean the inputs .strings
-        for (let e = 0; e < strings.length; e++) {
-            strings[e].value = '';
+            
+            let $bassTab = document.querySelector('.bass-tab');//<-- create by createBassTab() in scripts/createBassTab.js
+            
+            if (count === 1) {
+                addBassNotes($bassTab, strings, 'A');//<-- article inside of section ".tab-root", "strings", "second class" (important for create a second article and continue adding more notes on the tab)
+            }
+    
+            data.forEach(({ className, nextClassName, countState, id }) => {
+                if (document.querySelector(`.${className}`).textContent.length >= 41) {
+                    if (count === countState) {
+                    createBassTab($tabRoot, id);
+                    setCount(id)
+                    }
+                    addBassNotes(document.getElementById(id), strings, nextClassName);
+                }
+            });
+            // Clean the inputs .strings
+            for (let e = 0; e < strings.length; e++) {
+                strings[e].value = '';
+            }
+        } catch (error) {
+            console.error(error)
         }
+
     }
 
     return(
@@ -87,15 +89,13 @@ export function GuitarTabForm() {
     const sendNotes = () => {
         let strings = document.querySelectorAll('.strings');
         let $tabRoot = document.querySelector('.tab-root');
-
-        let $guitarTab;
         
         if (count === 0) {//<-- create the first $tabRoot without duplicating them
             createGuitarTab($tabRoot, 1);//<-- create an article and insert it inside $tabRoot
             setCount(1)
         }
         
-        $guitarTab = document.querySelector('.guitar-tab');//<-- create by createBassTab() in scripts/createBassTab.js
+        let $guitarTab = document.querySelector('.guitar-tab');//<-- create by createBassTab() in scripts/createBassTab.js
         
         if (count === 1) {
             addGuitarNotes($guitarTab, strings, 'A');//<-- article inside of section ".tab-root", "strings", "second class" (important for create a second article and continue adding more notes on the tab)
@@ -119,12 +119,12 @@ export function GuitarTabForm() {
     return(
         <>
             <form id="create-guitar-tab" className="from-create-tab flex gap-2 my-5 justify-center">
-                <input placeholder="e" type="number" name="e" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-6" min="0" max="24" />
-                <input placeholder="B" type="number" name="B" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-5" min="0" max="24" />
-                <input placeholder="G" type="number" name="G" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-4" min="0" max="24" />
-                <input placeholder="D" type="number" name="D" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-3" min="0" max="24" />
-                <input placeholder="A" type="number" name="A" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-2" min="0" max="24" />
-                <input placeholder="E" type="number" name="E" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-1" min="0" max="24" />
+                <input placeholder="e" type="text" name="e" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-6" min="0" max="24" />
+                <input placeholder="B" type="text" name="B" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-5" min="0" max="24" />
+                <input placeholder="G" type="text" name="G" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-4" min="0" max="24" />
+                <input placeholder="D" type="text" name="D" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-3" min="0" max="24" />
+                <input placeholder="A" type="text" name="A" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-2" min="0" max="24" />
+                <input placeholder="E" type="text" name="E" className="strings  w-16 h-8 border-solid border-x border-y border-black" id="string-1" min="0" max="24" />
                 <input onClick={sendNotes} id="sendNote-guitar" className="send-notes bg-orange-200 px-2 py-1 rounded hover:bg-orange-100" type="button" defaultValue="Send Tab" />
             </form>
             <section className="tab-root box-border border-solid border-x border-y border-black bg-slate-300 w-172 m-auto p-4"></section>

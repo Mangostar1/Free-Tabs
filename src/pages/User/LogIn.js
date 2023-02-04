8940693eff522bce8dc454fcb685ab7e5869f5c7
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-02-04 18:15:47 -0300

    Se agregan las rutas LogIn y SignUp y se crean sus respectivos componentes

---
+++ src/pages/User/LogIn.js
@@ -0,0 +1,7 @@
+export default function LogIn() {
+    return(
+        <>
+            <h1>Login page</h1>
+        </>
+    )
+}
--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from "pages/Home/Home";
 import YourTabs from "pages/YourTabs/YourTabs"
 import Error404Page from "pages/404/404";
+import LogIn from "pages/User/Login";
+import SignUp from "pages/User/SignUp";
 
 export default function PublicRoutes() {
     return(
@@ -13,7 +15,10 @@ export default function PublicRoutes() {
                 <ul className='flex justify-start gap-10'>
                     <Link className='text-xl text-white hover:text-orange-300' to='/'>Home</Link>
                     <Link className='text-xl text-white hover:text-orange-300' to='/tabs'>Your Tabs</Link>
-                    <Link className='text-xl text-white hover:text-orange-300' to='/asdasd'>404 Page</Link>
+                </ul>
+                <ul className='flex justify-start gap-10'>
+                    <Link className='text-xl text-white hover:text-orange-300' to='/user/signup'>Sign up</Link>
+                    <Link className='text-xl text-white hover:text-orange-300' to='/user/login'>Log in</Link>
                 </ul>
             </nav>
         </header>
@@ -21,6 +26,8 @@ export default function PublicRoutes() {
             <Routes>
                 <Route path='/' element={<Home />}/>
                 <Route path='/tabs' element={<YourTabs />}/>
+                <Route path='/user/signup' element={<SignUp />} />
+                <Route path='/user/login' element={<LogIn />} />
                 <Route path='*' element={<Error404Page />} />
             </Routes>
         </BrowserRouter>

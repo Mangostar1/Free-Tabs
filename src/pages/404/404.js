1b599d590ae6dfd72ef8b6b3669256d1b217efb0
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-02-09 17:35:42 -0300

    se agrega react icons, se cambia la forma en la que se muestra la tab guardada

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,7 +1,9 @@
+import { AiFillSetting } from "react-icons/ai";
+
 export default function Error404Page() {
     return(
         <>
-            <h1>Error 404, pagina no encontrada</h1>
+            <h1>Error 404, pagina no encontrada <AiFillSetting /></h1>
         </>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { AiFillSetting } from "react-icons/ai";
 //Components
 import { BassTabForm } from "pages/Home/BassTabForm";
 import { GuitarTabForm } from "pages/Home/GuitarTabForm";
@@ -9,38 +9,36 @@ import './styles/chooseIns.css'
 
 export default function Home() {
 
-    const [count, setCount] = useState(0);
-    
+    const [view, setView] = useState(0);
     const [tab, setTab] = useState();
-    const [html, setHtml] = useState();
     
-    const bass = () => {//<-- show Bass Tab
-        setCount(0);
+    const viewBassTab = () => {//<-- show Bass Tab
+        setView(0);
         document.querySelector('.tab-root').innerHTML = '';//<-- Tab Root come from TabForm.js line 29
     }
 
-    const guitar = () => {//<-- show Guitar Tab
-        setCount(1);
+    const viewGuitarTab = () => {//<-- show Guitar Tab
+        setView(1);
         document.querySelector('.tab-root').innerHTML = '';//<-- Tab Root come from TabForm.js line 29
     }
 
-    const save = () => {
+    const saveCurrentTab = () => {
         setTab(document.querySelector('.tab-root').outerHTML);
-        setHtml(<section className='test2' dangerouslySetInnerHTML={{ __html: tab }} />);//!<-- Quiza tenga que reemplazar el uso de dangerouslySetInnerHTML() en un futuro.
+        document.getElementById('Tab-Saved').innerHTML = tab;
     }
 
     return(
         <main className='grid grid-cols-2'>
             <form className='chooseIns'>{/* This form have custom styles in Home/styles/chooseIns.css */}
-                <input onChange={bass} type="radio" defaultValue="bass" name="instrument" className="choose-instrument" id="bass" defaultChecked />
+                <input onChange={viewBassTab} type="radio" defaultValue="bass" name="instrument" className="choose-instrument" id="bass" defaultChecked />
                 <label id='chooseBass' htmlFor="bass">Bass</label>
 
-                <input onChange={guitar} type="radio" defaultValue="guitar" name="instrument" className="choose-instrument" id="guitar" />
+                <input onChange={viewGuitarTab} type="radio" defaultValue="guitar" name="instrument" className="choose-instrument" id="guitar" />
                 <label id='chooseGuitar' htmlFor="guitar">Guitar</label>
             </form>
 
             <section className='test1'>
-                {count === 0 ? <BassTabForm/> : <GuitarTabForm/>}
+                {view === 0 ? <BassTabForm/> : <GuitarTabForm/>}
                 <div className='mt-5 ml-5'>
                     <h2 className='font-bold'>Glossary:</h2>
                     <ul className='ml-6'>
@@ -52,9 +50,9 @@ export default function Home() {
                         <li className='list-disc'>~  Vibrato</li>
                     </ul>
                 </div>
-                <button onClick={save} className='bg-orange-200 px-4 py-2 mt-5 ml-5 rounded hover:bg-orange-100'>Save</button>
+                <button onClick={saveCurrentTab} className='bg-orange-200 px-4 py-2 mt-5 ml-5 rounded hover:bg-orange-100'>Save</button>
             </section>
-            {html}
+            <section id='Tab-Saved'></section>
         </main>
     )
 }

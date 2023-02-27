524b65787f10907356ce6b24171f266e5365a53a
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-02-27 14:35:16 -0300

    Se instala redux, react redux, y se crea carpeta store para redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { store } from 'store/store'
+import { Provider } from 'react-redux'
 
 import './index.css';
 
@@ -8,6 +10,8 @@ import PublicRoutes from "routes/PublicRoutes";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PublicRoutes />
+    <Provider store={store}>
+      <PublicRoutes />
+    </Provider>
   </React.StrictMode>
 );

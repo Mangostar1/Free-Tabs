f6ace2f340bbbabc825f9c079e292fe5c8454645
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-03-06 13:55:32 -0300

    Se configura el login y signup, ahora ambos componentes funcionan correctamente y se pueden conectar al backend con exito.

--- src/pages/User/LogIn.jsx
+++ src/pages/User/LogIn.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 export default function LogIn() {
     
     const navigate = useNavigate();
-    const [body, setBody] = useState({username: '', password: ''});
+    const [body, setBody] = useState({email: '', password: ''});
 
     const handler = ({target}) => {
         const {name, value} = target;
@@ -19,7 +19,7 @@ export default function LogIn() {
         axios.post('https://my-backend-expressjs.up.railway.app/api/login', body)
         .then(() => {
             localStorage.setItem("auth", "yes");
-            navigate('/user/tabs');
+            navigate('/user/profile');
         })
         .catch(({response}) => {
             console.error(response.data, 'error!!')
@@ -29,7 +29,7 @@ export default function LogIn() {
     return(
         <div className="flex justify-center items-center h-screen">
             <form className="bg-slate-200 flex flex-col items-center justify-center gap-4 w-80 h-56 rounded">
-                <input type="text" name="username" placeholder="Username" value={body.username} onChange={handler} className="bg-slate-100 w-64 h-8" />
+                <input type="text" name="email" placeholder="E-mail" value={body.email} onChange={handler} className="bg-slate-100 w-64 h-8" />
                 <input type="password" name="password" placeholder="Password" value={body.password} onChange={handler} className="bg-slate-100 w-64 h-8" />
                 <input type="button" onClick={login} name="send" className="bg-orange-200 w-64 px-4 py-2 mt-2 rounded hover:bg-orange-100" value="Login"/>
             </form>
--- src/pages/User/SignUp.jsx
+++ src/pages/User/SignUp.jsx
@@ -1,20 +1,40 @@
-/* import { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios'; */
+import axios from 'axios';
 
 export default function SignUp() {
 
-    const signup = () => {//! API del Backend en construccion
-        console.log('Se registra un usuario - test');
+    const navigate = useNavigate();
+
+    const [body, setBody] = useState({email: '', password: ''});
+
+
+    const handler = ({target}) => {
+        const {name, value} = target;
+        setBody({
+            ...body,
+            [name]: value
+        })
+    }
+
+
+    const signup = () => {
+        axios.post('https://my-backend-expressjs.up.railway.app/api/signup', body)
+        .then(() => {
+            localStorage.setItem("login", "yes");
+            navigate('/');
+        })
+        .catch(({response}) => {
+            console.error(response.data, 'error!!')
+        })
     }
 
     return(
         <div className="flex justify-center items-center h-screen">
             <form className="bg-slate-200 flex flex-col items-center justify-center gap-4 w-80 h-72 rounded">
-                <input type="email" name="E-mail" placeholder="E-mail" className="bg-slate-100 w-64 h-8" />
-                <input type="text" name="username" placeholder="username" className="bg-slate-100 w-64 h-8" />
-                <input type="password" name="password" placeholder="Password" className="bg-slate-100 w-64 h-8" />
-                <input type="password" name="R-password" placeholder="R Password" className="bg-slate-100 w-64 h-8" />
+                <input type="email" value={body.email} onChange={handler} name="email" placeholder="E-mail" className="bg-slate-100 w-64 h-8" />
+                <input type="password" value={body.password} onChange={handler} name="password" placeholder="Password" className="bg-slate-100 w-64 h-8" />
+                {/* <input type="password" name="R-password" placeholder="R Password" className="bg-slate-100 w-64 h-8" /> */}
                 <input type="button" onClick={signup} name="send" className="bg-orange-200 w-64 px-4 py-2 mt-2 rounded hover:bg-orange-100" value="Sign up"/>
             </form>
         </div>

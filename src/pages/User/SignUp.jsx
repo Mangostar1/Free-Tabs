78e2f694e86871bf362bdb8dd13f50a9e88d3ab9
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-03-06 17:04:27 -0300

    Se agrega una expresion regular para controlar el minimo de caracteres requeridos, se activa el input re-password y se le agrega  una funcion para controlar que coinsida con el input name password.

--- src/pages/User/SignUp.jsx
+++ src/pages/User/SignUp.jsx
@@ -7,7 +7,9 @@ export default function SignUp() {
     const navigate = useNavigate();
 
     const [body, setBody] = useState({email: '', password: ''});
+    const [isEqual, setIsEqual] = useState(false);
 
+    const valid = /^[a-zA-Z0-9_@.-]{6,}$/;
 
     const handler = ({target}) => {
         const {name, value} = target;
@@ -15,27 +17,42 @@ export default function SignUp() {
             ...body,
             [name]: value
         })
+
+        if (valid.test(body.password)) {
+            console.log('La contraseña debe tener un minimo de 6 caracteres');
+        }
     }
 
+    const handleEqual = ({target}) => {
+        if (target.value === body.password) {
+            setIsEqual(true);
+        }
+
+        if (target.value !== body.password) {
+            console.log('Las contraseñas deben coincidir');
+        }
+    }
 
     const signup = () => {
-        axios.post('https://my-backend-expressjs.up.railway.app/api/signup', body)
-        .then(() => {
-            localStorage.setItem("login", "yes");
-            navigate('/');
-        })
-        .catch(({response}) => {
-            console.error(response.data, 'error!!')
-        })
+        if (isEqual === true) {
+            axios.post('https://my-backend-expressjs.up.railway.app/api/signup', body)
+            .then(() => {
+                localStorage.setItem("login", "yes");
+                navigate('/');
+            })
+            .catch(({response}) => {
+                console.error(response.data, 'error!!')
+            })
+        }
     }
 
     return(
         <div className="flex justify-center items-center h-screen">
             <form className="bg-slate-200 flex flex-col items-center justify-center gap-4 w-80 h-72 rounded">
-                <input type="email" value={body.email} onChange={handler} name="email" placeholder="E-mail" className="bg-slate-100 w-64 h-8" />
-                <input type="password" value={body.password} onChange={handler} name="password" placeholder="Password" className="bg-slate-100 w-64 h-8" />
-                {/* <input type="password" name="R-password" placeholder="R Password" className="bg-slate-100 w-64 h-8" /> */}
-                <input type="button" onClick={signup} name="send" className="bg-orange-200 w-64 px-4 py-2 mt-2 rounded hover:bg-orange-100" value="Sign up"/>
+                <input type="email" value={body.email} onChange={handler} name="email" placeholder="E-mail" className="pass bg-slate-100 w-64 h-8" required/>
+                <input type="password" value={body.password} onChange={handler} name="password" placeholder="Password" className="bg-slate-100 w-64 h-8" required/>
+                <input type="password" onChange={handleEqual} name="R-password" placeholder="R Password" className="bg-slate-100 w-64 h-8" required/>
+                <input type="button" onClick={signup} name="send" className="bg-orange-200 w-64 px-4 py-2 mt-2 rounded hover:bg-orange-100" value="Sign up"/>{/* add disabled */}
             </form>
         </div>
     )

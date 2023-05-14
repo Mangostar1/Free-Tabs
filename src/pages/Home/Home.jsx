af8c1a5086aa0f914cfb4dda1ed2bfa19903841a
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-05-13 20:05:40 -0400

    Se cambia el limite de la longitud maxima permitida en el createBassTab y BassTabForm a modo de prueba para corregir ciertos errores graficos de la app

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,94 +1,109 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 //components
-import CommonBtn from 'component/CommonBtn'
-import ImgMain from 'component/ImgMain'
-import HomeArticle from 'component/HomeArticle'
-
-
+import CommonBtn from "component/CommonBtn";
+import ImgMain from "component/ImgMain";
+import HomeArticle from "component/HomeArticle";
 
 export default function Home() {
-    
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  const goToCreateNewAcount = () => {
+    navigate("/user/login");
+  };
 
-    const goToCreateNewAcount = () => {
-        navigate('/user/login')
-    }
+  const goToCreateNewTab = () => {
+    navigate("/tab/create");
+  };
 
-    const goToCreateNewTab = () => {
-        navigate('/tab/create')
-    }
-
-    return(
-        <main className="bg-slate-50">
-            <section className="
+  return (
+    <main className="bg-slate-50">
+      <section
+        className="
                 bg-[url('assets/imgs/op-2.jpg')] bg-cover bg-no-repeat bg-[center_top_1rem] 
                 h-128 
                 flex flex-col items-center 
-                xl:bg-[center_top_-8rem]">
-                <h1 className='text-gray-100 text-8xl font-bold text-center mt-32'>Free Tabs</h1>
-                <div className='mt-10 flex flex-row gap-4'>
-                    <CommonBtn 
-                        handleBtn={goToCreateNewAcount}
-                        name="Create Acount" 
-                        classCss="text-gray-700 text-xl bg-orange-200 px-4 py-2 rounded-xl ease-out duration-500 hover:bg-orange-600 hover:text-gray-300"
-                    />
-                    <CommonBtn 
-                        handleBtn={goToCreateNewTab}
-                        name="Try Now" 
-                        classCss="text-gray-700 text-xl bg-orange-200 px-4 py-2 rounded-xl ease-out duration-500 hover:bg-orange-600 hover:text-gray-300"
-                    />
-                </div>
-            </section>
-            <section className='flex justify-center gap-4 bg-red-100 p-20'>
-                <ImgMain src="https://picsum.photos/800/400" alt="tab-guitar" classCss=""/>
-                <div className=''>
-                    <h2 className='text-center text-xl font-semibold'>Tus Tabs</h2>
-                    <p className='text-center m-auto'>
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-                        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-                        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. 
-                        Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, 
-                        venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
-                        elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend 
-                        ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, 
-                    </p>
-                </div>
-            </section>
-            <section className='flex flex-row justify-center items-center'>
-                <HomeArticle 
-                    title="Some Title 1" 
-                    text="
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam deserunt nam fugit dolorem labore commodi, 
-                    et velit eaque possimus. Architecto illum rerum veniam ipsam beatae dolore odio pariatur doloribus repellendus.
+                xl:bg-[center_top_-8rem]"
+      >
+        <h1 className="text-gray-100 text-8xl font-bold text-center mt-32">
+          Free Tabs
+        </h1>
+        <div className="mt-10 flex flex-row gap-4">
+          <CommonBtn
+            handleBtn={goToCreateNewAcount}
+            name="Create Acount"
+            classCss="text-gray-700 text-xl bg-orange-200 px-4 py-2 rounded-xl ease-out duration-500 hover:bg-orange-600 hover:text-gray-300"
+          />
+          <CommonBtn
+            handleBtn={goToCreateNewTab}
+            name="Try Now"
+            classCss="text-gray-700 text-xl bg-orange-200 px-4 py-2 rounded-xl ease-out duration-500 hover:bg-orange-600 hover:text-gray-300"
+          />
+        </div>
+      </section>
+      <section className="flex justify-center gap-4 p-20">
+        <ImgMain
+          src="https://picsum.photos/800/400"
+          alt="tab-guitar"
+          classCss=""
+        />
+        <div className="">
+          <h2 className="text-center text-xl font-semibold">Tus Tabs</h2>
+          <p className="text-center m-auto">
+            Toca tus canciones favoritas en tu guitarra o bajo con facilidad
+            utilizando nuestro creador de tablaturas. Crea, edita y comparte tus
+            propias tablaturas en cuestión de minutos. Con nuestro editor
+            intuitivo y herramientas avanzadas, cualquier persona puede crear
+            tablaturas de calidad profesional sin esfuerzo. Además, nuestra
+            biblioteca de tablaturas populares y actualizadas te ayudará a
+            descubrir nuevos temas para tocar. ¡Empieza a tocar como un
+            profesional hoy mismo!
+          </p>
+        </div>
+      </section>
+      <section className="flex flex-row justify-center items-center">
+        <HomeArticle
+          title="Some Title 1"
+          text="
+                    Crea tablaturas de alta calidad en minutos con nuestro editor intuitivo y herramientas avanzadas.
                     "
-                    cssArticle="flex flex-col m-4 p-4 rounded-md bg-indigo-100"
-                    cssTitle="text-xl font-semibold"
-                    cssText="" 
-                />
-                <HomeArticle 
-                    title="Some Title 2" 
-                    text="
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam deserunt nam fugit dolorem labore commodi, 
-                    et velit eaque possimus. Architecto illum rerum veniam ipsam beatae dolore odio pariatur doloribus repellendus.
+          cssArticle="flex flex-col m-4 p-4 rounded-md bg-indigo-100"
+          cssTitle="text-xl font-semibold"
+          cssText=""
+        />
+        <HomeArticle
+          title="Some Title 2"
+          text="
+                    Accede a una biblioteca de tablaturas actualizadas y populares para descubrir nuevas canciones para tocar.
                     "
-                    cssArticle="flex flex-col m-4 p-4 rounded-md bg-indigo-100"
-                    cssTitle="text-xl font-semibold"
-                    cssText="" 
-                />
-                <HomeArticle 
-                    title="Some Title 3" 
-                    text="
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam deserunt nam fugit dolorem labore commodi, 
-                    et velit eaque possimus. Architecto illum rerum veniam ipsam beatae dolore odio pariatur doloribus repellendus.
+          cssArticle="flex flex-col m-4 p-4 rounded-md bg-indigo-100"
+          cssTitle="text-xl font-semibold"
+          cssText=""
+        />
+        <HomeArticle
+          title="Some Title 3"
+          text="
+                    Comparte tus tablaturas con otros músicos y colabora en proyectos musicales.
                     "
-                    cssArticle="flex flex-col m-4 p-4 rounded-md bg-indigo-100"
-                    cssTitle="text-xl font-semibold"
-                    cssText="" 
-                />
-            </section>
-        </main>
-    )
+          cssArticle="flex flex-col m-4 p-4 rounded-md bg-indigo-100"
+          cssTitle="text-xl font-semibold"
+          cssText=""
+        />
+      </section>
+      <footer>
+        <div className="flex gap-4 justify-center py-8">
+          <a href="https://www.google.com/">
+            <img src="https://picsum.photos/35/35" alt="social-media" />
+          </a>
+          <a href="https://www.google.com/">
+            <img src="https://picsum.photos/35/35" alt="social-media" />
+          </a>
+          <a href="https://www.google.com/">
+            <img src="https://picsum.photos/35/35" alt="social-media" />
+          </a>
+        </div>
+      </footer>
+    </main>
+  );
 }
--- src/pages/Tabs/scripts/createBassTab.js
+++ src/pages/Tabs/scripts/createBassTab.js
@@ -1,51 +1,49 @@
 let $strings;
-export const scaleNotes = ['G', 'D', 'A', 'E'];
+export const scaleNotes = ["G", "D", "A", "E"];
 
 export function createBassTab(element, id) {
-    
-    const $bassTab = document.createElement('div');
-    $bassTab.classList.add("bass-tab");//<-- main class
-    $bassTab.id = id;
+  const $bassTab = document.createElement("div");
+  $bassTab.classList.add("bass-tab"); //<-- main class
+  $bassTab.id = id;
 
-    element.appendChild($bassTab);
-
+  element.appendChild($bassTab);
 }
 
 /* This function create <p> element and insert tab notes inside */
 export function addBassNotes(element, num, idClass) {
-    
-    if (!element.hasChildNodes()) {//<-- first print on screen
-        
-        for (let i = 0; i < scaleNotes.length; i++) {
+  if (!element.hasChildNodes()) {
+    //<-- first print on screen
 
-            const bassStrings = document.createElement('p');//<-- create <p> the element
-            bassStrings.classList.add('bass-strings', idClass);//<-- add class
-            
-            bassStrings.textContent += `${scaleNotes[i]} ——${num[i].value === "" ? "—" : num[i].value}`;
-            
-            element.appendChild(bassStrings);
+    for (let i = 0; i < scaleNotes.length; i++) {
+      const bassStrings = document.createElement("p"); //<-- create <p> the element
+      bassStrings.classList.add("bass-strings", idClass); //<-- add class
 
-        }
+      bassStrings.textContent += `${scaleNotes[i]} —${
+        num[i].value === "" ? "—" : num[i].value
+      }`;
 
-    } else {
-        
-        //Select second class to work without bugs when you have more than one ".bass-tab" article
-        $strings = document.querySelectorAll(`.${idClass}`);
-    
-        for (let i = 0; i < $strings.length; i++) {
-            
-            if ($strings[i].textContent.length >= 41) {
-                console.log('%cNo more tabs on this article .bass-tab', 'background-color:yellow; color:#000; font-size:20px;');
-                break;
-            } else {
-                $strings[i].textContent += `——${num[i].value === "" ? "—" : num[i].value}`;
-            }
-            
-        }
+      element.appendChild(bassStrings);
     }
+  } else {
+    //Select second class to work without bugs when you have more than one ".bass-tab" article
+    $strings = document.querySelectorAll(`.${idClass}`);
 
+    for (let i = 0; i < $strings.length; i++) {
+      if ($strings[i].textContent.length > 40) {
+        console.log(
+          "%cNo more tabs on this article .bass-tab",
+          "background-color:yellow; color:#000; font-size:20px;"
+        );
+        break;
+      } else {
+        $strings[i].textContent += `—${
+          num[i].value === "" ? "—" : num[i].value
+        }`;
+      }
+    }
+  }
 }
 
-export function addEditNotes(element) {
+/* export function addEditNotes(element) {
     
-}
+} */

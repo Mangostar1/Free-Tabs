7a7df5337c67d0f1e5b5408a9bbffb582b359ffd
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-06-01 17:28:10 -0400

    Se arregla error: ahora los link de las rutas privadas ya no tienen margen/padding

--- src/component/UserLinks.jsx
+++ src/component/UserLinks.jsx
@@ -8,7 +8,7 @@ export default function UserLinks({ route, nameRoute }) {
   if (isAuthenticated) {
     return (
       <Link
-        className="px-2 py-2 text-sm text-gray-500 lg:px-6 md:px-3 hover:text-blue-600 lg:ml-auto"
+        className="px-2 py-2 text-sm text-gray-500 lg:px-6 md:px-3 hover:text-blue-600"
         to={route}
       >
         {nameRoute}

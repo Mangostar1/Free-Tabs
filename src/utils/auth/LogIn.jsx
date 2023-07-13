06cb1efed81d79057fab2f2f0bf7919f0b0f8214
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-07-13 15:46:18 -0400

    Se termina de dar estilos al componente LogIn

--- src/utils/auth/LogIn.jsx
+++ src/utils/auth/LogIn.jsx
@@ -38,36 +38,60 @@ export default function LogIn() {
   };
 
   return (
-    <main className="">
-      <form
-        onSubmit={loginHandler}
-        className="bg-neutral-50 flex flex-col items-center justify-center gap-4 px-2 py-4"
-      >
-        <input
-          type="text"
-          name="email"
-          placeholder="E-mail"
-          value={body.email}
-          onChange={handler}
-          className="block w-52 px-3 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          autoComplete="off"
-          value={body.password}
-          onChange={handler}
-          className="block w-52 px-3 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-        />
-        <input
-          type="submit"
-          ref={submitButtonRef}
-          name="send"
-          className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-orange-400 border-2 border-orange-400 rounded-full inline-flex hover:bg-orange-200 hover:border-orange-400 hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black"
-          value="Login"
-        />
-      </form>
+    <main>
+      <section className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-20 max-w-7xl">
+        <article className="w-full max-w-md mx-auto md:max-w-sm md:px-0 md:w-96 sm:px-4">
+          <div className="flex flex-col">
+            <div>
+              <h2 className="text-4xl text-black">Login</h2>
+            </div>
+          </div>
+          <form onSubmit={loginHandler}>
+            <div className="mt-4 space-y-6">
+              <div className="col-span-full">
+                <label
+                  className="block mb-3 text-sm font-medium text-gray-600"
+                  name="password"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="E-mail"
+                  value={body.email}
+                  onChange={handler}
+                  className="block w-full px-6 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
+                />
+              </div>
+              <div className="col-span-full">
+                <label
+                  className="block mb-3 text-sm font-medium text-gray-600"
+                  name="password"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  autoComplete="off"
+                  value={body.password}
+                  onChange={handler}
+                  className="block w-full px-6 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
+                />
+              </div>
+              <input
+                type="submit"
+                ref={submitButtonRef}
+                name="send"
+                className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-orange-400 border-2 border-orange-400 rounded-full inline-flex hover:bg-orange-200 hover:border-orange-400 hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black"
+                value="Login"
+              />
+            </div>
+          </form>
+        </article>
+      </section>
     </main>
   );
 }

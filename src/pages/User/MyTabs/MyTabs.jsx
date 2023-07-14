db160ab7d22284f8f0e91209aa19ed3484470336
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-07-13 21:15:06 -0400

    Feat: - Se comiensan trabajos en ruta MyTabs.

--- src/pages/User/MyTabs/MyTabs.jsx
+++ src/pages/User/MyTabs/MyTabs.jsx
@@ -1,7 +1,37 @@
-export default function MyTabs(params) {
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
+import { endpoint } from "utils/urlApi";
+
+export default function MyTabs() {
+  //Cookie
+  const jwtToken = Cookies.get("jwtToken");
+
+  const getTabs = () => {
+    axios.defaults.withCredentials = true;
+    axios.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${jwtToken}`;
+      return config;
+    });
+    axios
+      .get(endpoint.getUserTab)
+      .then((response) => {
+        console.info("Se hace el get a firestore");
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
   return (
     <main className="bg-slate-50 min-h-screen">
-      <h1>User Tabs</h1>
+      <h1 className="">User Tabs</h1>
+      <input
+        type="button"
+        className="bg-orange-400 px2 py4"
+        value="Get Tabs"
+        onClick={getTabs}
+      />
     </main>
   );
 }

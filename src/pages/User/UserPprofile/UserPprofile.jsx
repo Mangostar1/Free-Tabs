6cda6898bf796156e0fc28e339d24a303dff23e4
Omar <93488662+Mangostar1@users.noreply.github.com>
2024-02-28 14:07:35 -0300

    Se modifican estilos de la pagina UserPprofile. para dejar mas completa la seccion de info del usuario.

--- src/pages/User/UserPprofile/UserPprofile.jsx
+++ src/pages/User/UserPprofile/UserPprofile.jsx
@@ -6,6 +6,10 @@ import Swal from 'sweetalert2';
 
 //Material UI
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 //SVG
 import cameraSvg from "assets/icons/camera.svg";
@@ -120,68 +124,108 @@ export default function UserProfile(props) {
           </div>
         ) : (
           // Mostrar el contenido una vez que la data se ha cargado
-          <section className="flex flex-row justify-between py-14 px-10 m-auto w-144 relative">
-            <article className="absolute top-5 right-8">
-              <Button variant="contained" onClick={handleEdit} size="small">Editar</Button>
-            </article>
-
-            <article className="flex flex-col gap-5">
-              <div className="">
-                <h2 className="text-gray-700 font-bold">Nombre</h2>
-                {editMode === false ? (
-                  <p className="text-gray-900 font-medium tracking-widest">
-                    {userdata.userName}
-                  </p>
-                ) : (
-                  <input
-                    type="text"
-                    name="userName"
-                    placeholder={userdata.userName}
-                    value={userdata.userName}
-                    className="w-full tracking-widest"
-                    onChange={handler}
+          <>
+            <section className="flex flex-row justify-between py-5 px-10 m-auto relative">
+              <article className="absolute top-5 right-8">
+                <Button variant="contained" onClick={handleEdit} size="small">Editar</Button>
+              </article>
+              <article className="relative">
+                  <img
+                    src={userImage}
+                    width='100'
+                    height='100'
+                    alt="profile_img"
+                    className="rounded-full"
                   />
-                )}
+                  {editMode === true ? (
+                    <button
+                      onClick={handleAvatar}
+                      title="change avatar"
+                      className="transition absolute bottom-0 right-0 w-10 h-10 shadow-md bg-white rounded-full flex justify-center items-center hover:bg-gray-100"
+                    >
+                      <img src={cameraSvg} alt="change_avatar" className="w-7 h-7" />
+                    </button>
+                  ) : null}
+                </article>
+            </section>
+            <section className="flex flex-row justify-between py-5 px-10 m-auto relative">
+              <article className="flex flex-row gap-5">
+                <div className="mx-5">
+                  <h2 className="text-gray-700 font-bold">Nombre</h2>
+                  {editMode === false ? (
+                    <p className="text-gray-900 font-medium tracking-widest">
+                      {userdata.userName}
+                    </p>
+                  ) : (
+                    <input
+                      type="text"
+                      name="userName"
+                      placeholder={userdata.userName}
+                      value={userdata.userName}
+                      className="w-full tracking-widest"
+                      onChange={handler}
+                    />
+                  )}
 
-                {editMode === false ? (
-                  <div className="w-full h-0.5 bg-gray-400"></div>
-                ) : (
-                  <div className="w-full h-0.5 bg-orange-400"></div>
-                )}
-              </div>
-              <div className="">
-                <h2 className="text-gray-700 font-bold">E-MAIL</h2>
-                <p className="text-gray-900 font-medium tracking-widest">
-                  {userdata.email}
-                </p>
-                <div className="w-full h-0.5 bg-gray-400"></div>
-              </div>
-              {editMode === true ? (
-                <div className="flex flex-row gap-4">
-                  <Button variant="contained" onClick={saveChanges} title="Guardar Cambios" size="small">Guardar</Button>
-                  <Button variant="contained" onClick={cancelChanges} size="small">Cancelar</Button>
+                  {editMode === false ? (
+                    null
+                  ) : (
+                    <div className="w-full h-0.5 bg-orange-400"></div>
+                  )}
+
+                  <h2 className="text-gray-700 font-bold mt-5">E-MAIL</h2>
+                  <p className="text-gray-900 font-medium tracking-widest">
+                    {userdata.email}
+                  </p>
+
+                  <h2 className="text-gray-700 font-bold mt-5">Descripción</h2>
+                  <p className="text-gray-900 tracking-widest">
+                    Descripcion del usuario...
+                  </p>
+
                 </div>
-              ) : null}
-            </article>
-            <article className="relative">
-              <img
-                src={userImage}
-                width='200'
-                height='200'
-                alt="profile_img"
-                className="rounded-full"
-              />
+                <div className="">
+                  <p className="text-gray-900 bg-[#ffac33] font-medium tracking-widest py-1 px-2 rounded">
+                    ROL
+                  </p>
+                </div>
+                {editMode === true ? (
+                  <div className="flex flex-row gap-4">
+                    <Button variant="contained" onClick={saveChanges} title="Guardar Cambios" size="small">Guardar</Button>
+                    <Button variant="contained" onClick={cancelChanges} size="small">Cancelar</Button>
+                  </div>
+                ) : null}
+              </article>
+            </section>
+            <section className="flex flex-col gap-4 justify-between py-5 px-10 m-auto relative">
               {editMode === true ? (
-                <button
-                  onClick={handleAvatar}
-                  title="change avatar"
-                  className="transition absolute bottom-0 right-4 w-11 h-11 shadow-md bg-white rounded-full flex justify-center items-center hover:bg-gray-100"
-                >
-                  <img src={cameraSvg} alt="change_avatar" className="w-7 h-7" />
-                </button>
-              ) : null}
-            </article>
-          </section>
+                  <div className="flex flex-row gap-4">
+                    <Button variant="contained" onClick={saveChanges} title="Guardar Cambios" size="small">Guardar</Button>
+                    <Button variant="contained" onClick={cancelChanges} size="small">Cancelar</Button>
+                  </div>
+                ) : <>
+                  <article>
+                    <Link target="_blank" href="https://www.facebook.com/" underline="hover" color="inherit">
+                      <FacebookIcon />
+                      <span className="pl-2">Some User</span>
+                    </Link>
+                  </article>
+                  <article>
+                    <Link target="_blank" href="https://www.instagram.com/" underline="hover" color="inherit">
+                      <InstagramIcon />
+                      <span className="pl-2">Some User</span>
+                    </Link>
+                  </article>
+                  <article>
+                    <Link target="_blank" href="https://twitter.com/" underline="hover" color="inherit">
+                      <XIcon />
+                      <span className="pl-2">Some User</span>
+                    </Link>
+                  </article>
+                </>
+              }
+            </section>
+          </>
         )}
       </main>
       <Footer />
